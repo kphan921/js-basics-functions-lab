c23986434defc1c76864d7b330eb81ad9bd994a4
Tri <triphan921@gmail.com>
2021-02-22 16:47:08 -0800

    commit tree a7a19b3954fbdb13bd75f8c1948b8847266f4789

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(n) {
+    return Math.abs(42 - n);
+};
+
+function distanceFromHqInFeet(n) {
+    return distanceFromHqInBlocks(n) * 264;
+}
+
+const distanceTravelledInFeet = (num1, num2) => Math.abs(num1 - num2) * 264;
+
+const calculatesFarePrice = (start, destination) => {
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 400 && distance <= 2000) {
+        return 0.02 * (distance - 400);
+    } else if (distance > 2500) {
+        return `cannot travel that far`;
+    } else if (distance > 2000) {
+        return 25;
+    }
+}
